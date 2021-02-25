27f539508bb3fc8d3162438baf5333f0bfef38e3
Kyle Liss <kliss0093@myduneland.org>
2021-02-25 09:25:51 -0600

    just fixed up some things in the Loan class

--- Loan.java
+++ Loan.java
@@ -31,15 +31,16 @@ public class Loan
     public double getLoanAmount(){
         return loanAmount;
     }
+    
     //HC
     public int getNumLenders(){
         return numLenders;
     }
+    
     //HC
     public String toString(){
         return ID+" "+loanAmount+" "+country+" "+daysToFund+" "+numLenders+" ";
     }
-    
     
     //AM
     public String getCountry(){
@@ -56,9 +57,11 @@ public class Loan
         this.ID = ID;
     }
     
+    //KL
     public void setLoanAmount(double loanAmount){
         this.loanAmount = loanAmount;
     }
+    
     //AM
      public void setCountry(String newCountry){
         country = newCountry;
@@ -68,6 +71,7 @@ public class Loan
     public void setDaysToFund(int newFund){
         daysToFund = newFund;
     }
+    
     //HC
     public void setNumLenders(int newnumLenders){
         numLenders = newnumLenders;
