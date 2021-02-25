9ecf4e84b597199a09fcb34ec22b8d002985d453
Aidan <amoran0861@myduneland.org>
2021-02-25 08:46:59 -0600

    bluej update

---
+++ Loan.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class Loan here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Loan
+{
+    // instance variables - replace the example below with your own
+    private int ID;
+    private double loanAmount;
+    private String country;
+    private int daysToFund;
+    private int numLenders;
+
+    public Loan(int ID, double loanAmount, String country, int daysToFund, 
+            int numLenders) {
+     this.ID = ID;
+     this.loanAmount = loanAmount;
+     this.country = country;
+     this.daysToFund = daysToFund;
+     this.numLenders = numLenders;
+    }
+    
+    public int getID(){
+        return ID;
+    }
+    
+    public double getLoanAmount(){
+        return loanAmount;
+    }
+    
+    public String getCountry(){
+        return country;
+    }
+    
+    public int getDaysToFund(){
+        return daysToFund;
+    }
+}
